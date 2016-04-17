0899ecd8124d98b19520f69765689c2b758f46e5
Andrew Jiang <andrewji@usc.edu>
2016-04-16 20:41:42 -0700

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var Firebase = require('firebase');
+var async = require('async');
+var db = new Firebase("https://hackschedule.firebaseio.com/schedules");
+
+//var TROJAN = require('./lib/TROJAN');
+var api = require('./lib/api');
+
+app.use(require('express-promise')());
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/api/:method.:action', api);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
