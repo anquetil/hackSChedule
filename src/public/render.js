973bb4da037178f6191a4096e65cb742eb1619cf
Andrew Jiang <andrewji@usc.edu>
2017-03-22 09:33:16 -0700

    i give up on elastic beanstalk

--- src/public/render.js
+++ src/public/render.js
@@ -5,6 +5,12 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import Landing from './views/Landing';
 import Scheduler from './views/Scheduler';
 
+// https only
+var loc = window.location.href+'';
+if (loc.indexOf('http://')==0){
+    window.location.href = loc.replace('http://','https://');
+}
+
 render(
   <Router history={browserHistory}>
     <Route path='/'>
